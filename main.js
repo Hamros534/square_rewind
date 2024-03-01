cf53e0e51d367132460b3c60fc73856b26203632
Egor Gandziy <gandziygor@gmail.com>
2024-03-01 21:53:15 +0200

    beta v0.1

---
+++ main.js
@@ -0,0 +1,7 @@
+function like_click(element) {
+    if (element.style.backgroundColor === "rgb(255, 68, 68)") 
+        element.style.backgroundColor = "#323232";
+    else 
+        element.style.backgroundColor = "#ff4444";
+    
+}
