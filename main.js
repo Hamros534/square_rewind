b7e7b6badd1e83c0722af8aa52a10ade3b6825a7
Egor Gandziy <gandziygor@gmail.com>
2024-03-02 10:11:26 +0200

    beta v0.1

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-
-
 function like_click(element) {
     var img = element.querySelector('img');
     var filename = img.src.substring(img.src.lastIndexOf('/') + 1);
 
-    if (filename == 'heart.png') 
+    if (filename == 'heart.png') {
+        console.log(filename);
         img.src = 'img/filled_heart.png';
+    }
     else {
         img.src = 'img/heart.png';
         img.style.opacity = 1;
